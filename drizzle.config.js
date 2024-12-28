17f0936e818520aa192704b2d2766604c2845d87
samuel maveke <mavekepeter2022@gmail.com>
2024-12-28 21:58:13 +0300

    pushed all schemas to drizzle neon db

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default{
+    dialect:'postgresql',
+    schema:'./utils/schema.jsx',
+    out:'./drizzle',
+    dbCredentials:{
+        url:'postgresql://ai-room-redesign_owner:s9GcKpaRmxe6@ep-crimson-band-a5ev7oz0.us-east-2.aws.neon.tech/finan-smart?sslmode=require',
+        connectionnStrings:'postgresql://ai-room-redesign_owner:s9GcKpaRmxe6@ep-crimson-band-a5ev7oz0.us-east-2.aws.neon.tech/finan-smart?sslmode=require',
+    }
+
+
+}
